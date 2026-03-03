aa7efa050c79a133428fce7bf448e0d26de09507
Asadullah <asadullahyounas1437@gmail.com>
2026-03-03 19:15:25 +0500

    Refactor array sorting to only ascending order
    Removed descending order sorting and cleaned up code.

--- Arrays (1D + 2D + Nested)/06- Order the array in ascending order( small - greater).cpp
+++ Arrays (1D + 2D + Nested)/06- Order the array in ascending order( small - greater).cpp
@@ -1,63 +1,35 @@
+
 #include <iostream>
-using namespace std;
+ using namespace std;
+ 
 
-int main()
+int main() {
+    // Write C++ code here
+   int arr[10];
+   int n;
+   cout<<"\n Enter the size of the array::"<<endl;
+   cin>>n;
+   for( int i=0; i<n;i++)
+   {
+       cin>>arr[i];
+   }
+//   sort array in ascending order
+for ( int i=0;i<n;i++)
 {
-    int n;
-
-    cout << "Enter the size of array: ";
-    cin >> n;
-
-    int arr[n];
-
-    cout << "Enter " << n << " elements:\n";
-    for(int i = 0; i < n; i++)
+    for ( int j=i+1;j<n; j++)
     {
-        cin >> arr[i];
-    }
-
-    // Sorting in Ascending Order
-    for(int i = 0; i < n-1; i++)
-    {
-        for(int j = 0; j < n-1-i; j++)
+        if( arr[i]>arr[j])
         {
-            if(arr[j] > arr[j+1])
-            {
-                // Swap
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+            int temp= arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
         }
     }
-    // Sorting in Descending Order
-    for(int i = 0; i < n-1; i++)
-    {
-        for(int j = 0; j < n-1-i; j++)
-        {
-            if(arr[j] <arr[j+1])
-            {
-                // Swap
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-     cout << "Array in Ascending Order:\n";
-    for(int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-
-
-    cout << "\nArray in descending Order:\n";
-    for(int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-
+}
+cout<<"After sorting the array::"<<endl;
+for( int i=0;i<n; i++)
+{
+    cout<<arr[i]<< "   ";
+}
     return 0;
 }
-
-
