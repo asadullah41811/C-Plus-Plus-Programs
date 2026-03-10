92f7233390145ada69023cbd6d6c73a6f2f09c61
Asadullah <asadullahyounas1437@gmail.com>
2026-03-11 00:30:17 +0500

    uploaded the pattern using the 2D array

---
+++ Star and Pattern/08-Print the pattern using 2D array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+//    Print the pattern using 2D pattern
+cout<<" Pattern using the 2D Array"<<endl;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
